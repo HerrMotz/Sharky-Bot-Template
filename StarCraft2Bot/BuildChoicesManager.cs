d3d60375f0914100b2c8ce51ff1aff77396e3461
Daniel Motz <info@danielmotz.de>
2023-11-22 12:22:13 +0100

    Rename Opener

--- StarCraft2Bot/BuildChoicesManager.cs
+++ StarCraft2Bot/BuildChoicesManager.cs
@@ -39,18 +39,18 @@ namespace StarCraft2Bot
             //    ["Transition"] = defaultSequences
             //};
 
-            var reaperCheese = new TvTOpener(defaultSharkyBot);
+            var tvtOpener = new TvTOpener(defaultSharkyBot);
             var reaperCheese2 = new SaltyMarines(defaultSharkyBot, scvMicroController);
 
             var builds = new Dictionary<string, ISharkyBuild>
             {
-                [reaperCheese.Name()] = reaperCheese,
+                [tvtOpener.Name()] = tvtOpener,
                 [reaperCheese2.Name()] = reaperCheese2,
             };
 
             var defaultSequences = new List<List<string>>
             {
-                new List<string> { currentOpener.Name() },
+                new List<string> { tvtOpener.Name() },
             };
 
             var defaultSequences2 = new List<List<string>>
@@ -58,11 +58,12 @@ namespace StarCraft2Bot
                 new List<string> { reaperCheese2.Name() },
             };
 
+
             // INFO: The "Transition" entry should usually contain something other than the same builds over again
             var buildSequences = new Dictionary<string, List<List<string>>>
             {
                 [Race.Terran.ToString()] = defaultSequences,
-                ["Transition"] = defaultSequences2
+                // ["Transition"] = defaultSequences2
             };
 
             return new BuildChoices { Builds = builds, BuildSequences = buildSequences };
